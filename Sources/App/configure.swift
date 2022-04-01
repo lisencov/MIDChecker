8b10c3f4d131ee9398e707e2a540620a526d2352
slisenkov <slisenkov@ivi.ru>
2022-04-01 17:39:07 +0600

    Remove proxy

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,6 +6,5 @@ public func configure(_ app: Application) throws {
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
     // register routes
-    app.http.client.configuration.proxy = .server(host: "192.168.0.103", port: 8888)
     try routes(app)
 }
